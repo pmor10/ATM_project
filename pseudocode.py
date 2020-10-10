1a31068ea7aa8ef82cd3df987b5339c6d659e6f7
Paresa Morton <43301682+pmor10@users.noreply.github.com>
2020-10-09 21:45:15 -0700

    Add files via upload

---
+++ pseudocode.py
@@ -0,0 +1,42 @@
+# Greet User:
+# Display welcome message
+# Display instructions for how to use the program
+	# enter account id 
+	# create an account
+# Ask user what the want to do
+
+# Create an account
+	# Create int data field named id for the account 
+	# Create float data field named balance
+	# Create string data filed named first name/last name
+	# Create int data field named phone 
+
+# Enter account id
+	# if the id is entered incorrectly, ask the user to enter a correct id
+	# if the id is entered correctly display the main menu
+
+# Display the main menu
+# Ask the user what they want to do
+# User can enter choice of 1 for viewing the current balance
+# 2 for withdrawing money
+# 3 for depositing money
+# 4 for existing the main menu
+
+	# 1- Check Balance
+		# if user chooses 1
+			# display balance
+			# main menu
+	# 2- Withdraw
+		# if user chooses 2
+			# ask the amount
+			# if amount requested is greater than balance display cannot withdraw.
+			# if amount requested is less than balance display Withdrawing amount and 			current balance.
+
+	# 3- Deposit
+		# if user chooses 3
+			# ask the amount
+			# display deposit amount and the current balance.
+	# 4- Exit
+
+
+# Once user exit the system will prompt enter an id or create an account options
